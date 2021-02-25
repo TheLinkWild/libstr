bbeade9826954ef6a8bd7cef3876c456a684c63a
Vedqiibyol <77901580+TheLinkWild@users.noreply.github.com>
2021-02-25 01:03:43 +0100

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# version 0.2.0-dev
+
+echo "Installing to « /usr/include/ »..."
+cp src/libstr /usr/include/
+cp src/libstr_heads/ /usr/include/
+
+echo "Installation done!"
+echo "Thank you for using libstr"
+echo "version 0.2.0-dev | Vedqiibyol https://github.com/TheLinkWild/libstr"
